4665e7ece123bbd57e81864d0009f4732a9304a1
zhuo zheng <zhuozheng@zhuos-MBP.fios-router.home>
2018-09-01 13:16:35 -0400

    modify map style

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -66,171 +66,113 @@ class Map extends Component {
 
         const dayMapType = new google.maps.StyledMapType(
             [
-               {
-                    "featureType": "all",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "weight": "2.00"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "all",
-                    "elementType": "geometry.stroke",
-                    "stylers": [
-                        {
-                            "color": "#9c9c9c"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "all",
-                    "elementType": "labels.text",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
                 {
                     "featureType": "landscape",
-                    "elementType": "all",
                     "stylers": [
                         {
-                            "color": "#f2f2f2"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape.man_made",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "saturation": -100
+                            "hue": "#FFBB00"
                         },
                         {
-                            "lightness": 45
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "geometry.fill",
-                    "stylers": [
+                            "saturation": 43.400000000000006
+                        },
                         {
-                            "color": "#eeeeee"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
+                            "lightness": 37.599999999999994
+                        },
                         {
-                            "color": "#7b7b7b"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "labels.text.stroke",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
+                            "gamma": 1
                         }
                     ]
                 },
                 {
                     "featureType": "road.highway",
-                    "elementType": "all",
                     "stylers": [
                         {
-                            "visibility": "simplified"
+                            "hue": "#FFC200"
+                        },
+                        {
+                            "saturation": -61.8
+                        },
+                        {
+                            "lightness": 45.599999999999994
+                        },
+                        {
+                            "gamma": 1
                         }
                     ]
                 },
                 {
                     "featureType": "road.arterial",
-                    "elementType": "labels.icon",
                     "stylers": [
                         {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "transit",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "water",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "color": "#46bcec"
+                            "hue": "#FF0300"
                         },
                         {
-                            "visibility": "on"
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 51.19999999999999
+                        },
+                        {
+                            "gamma": 1
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {"visibility": "off"}
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "hue": "#FF0300"
+                        },
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 52
+                        },
+                        {
+                            "gamma": 1
                         }
                     ]
                 },
                 {
                     "featureType": "water",
-                    "elementType": "geometry.fill",
                     "stylers": [
                         {
-                            "color": "#c8d7d4"
+                            "hue": "#0078FF"
+                        },
+                        {
+                            "saturation": -13.200000000000003
+                        },
+                        {
+                            "lightness": 2.4000000000000057
+                        },
+                        {
+                            "gamma": 1
                         }
                     ]
                 },
                 {
-                    "featureType": "water",
-                    "elementType": "labels.text.fill",
+                    "featureType": "poi",
+                    "elementType": "labels",
                     "stylers": [
                         {
-                            "color": "#070707"
+                            "visibility": "off"
                         }
                     ]
                 },
                 {
-                    "featureType": "water",
-                    "elementType": "labels.text.stroke",
+                    "featureType": "poi.park",
+                    "elementType": "geometry",
                     "stylers": [
                         {
-                            "color": "#ffffff"
+                            "color": "#bde6ab"
                         }
                     ]
                 }
